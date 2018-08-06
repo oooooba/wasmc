4872a9cd852bf91be08feb42022b8543eb2cc0c0
oooooba <fuumin.23@gmail.com>
2018-08-06 21:35:53 +0900

    feature: add unsigned LEB 128 decoder to parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,10 +3,46 @@ use std::io::{BufReader, Read};
 
 use wasmir::Module;
 
-#[derive(Debug)]
+#[derive(PartialEq, Eq, Debug)]
 pub enum ParserErrorKind {
     FileNotFound(String),
+    FileTerminated,
     InvalidFormat(String),
+}
+
+fn decode_by_unsigned_leb128(reader: &mut Read, mut nbits: usize) -> Result<usize, ParserErrorKind> {
+    assert!(nbits == 32 || nbits == 64);
+    let num_iters = (nbits as f64 / 7.0).ceil() as usize;
+    let mut result: usize = 0;
+    for i in 0..num_iters {
+        let mut buf = [0];
+        let n = match reader.read(&mut buf) {
+            Ok(1) => buf[0],
+            _ => return Err(ParserErrorKind::FileTerminated),
+        };
+        result |= (n as usize & 0x7f) << (i * 7);
+        if n & 0x80 == 0 {
+            break
+        }
+        nbits -= 8;
+    }
+    Ok(result)
+}
+
+#[test]
+fn test_integer_decoder() {
+    let mut buf: &[u8] = &[0x02];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(2));
+    let mut buf: &[u8] = &[0x7f];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(127));
+    let mut buf: &[u8] = &[0x80, 0x01];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(128));
+    let mut buf: &[u8] = &[0x81, 0x01];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(129));
+    let mut buf: &[u8] = &[0x82, 0x01];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(130));
+    let mut buf: &[u8] = &[0xb9, 0x64];
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(12857));
 }
 
 pub fn parse(file_name: String) -> Result<Module, ParserErrorKind> {
