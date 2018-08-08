34a2fc6689af9ac618409a551c9f9347a063311b
oooooba <fuumin.23@gmail.com>
2018-08-08 16:10:50 +0900

    refactoring: fix LEB 128 decode function to return number of consumed bytes

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -18,35 +18,37 @@ fn read_one_byte(reader: &mut Read) -> Option<u8> {
     }
 }
 
-fn decode_by_unsigned_leb128(reader: &mut Read, mut nbits: usize) -> Result<usize, ParserErrorKind> {
+fn decode_by_unsigned_leb128(reader: &mut Read, mut nbits: usize) -> Result<(usize, usize), ParserErrorKind> {
     assert!(nbits == 32 || nbits == 64);
     let num_iters = (nbits as f64 / 7.0).ceil() as usize;
     let mut result: usize = 0;
+    let mut consumed = 0;
     for i in 0..num_iters {
         let n = read_one_byte(reader).ok_or(ParserErrorKind::FileTerminated)?;
+        consumed += 1;
         result |= (n as usize & 0x7f) << (i * 7);
         if n & 0x80 == 0 {
             break
         }
         nbits -= 8;
     }
-    Ok(result)
+    Ok((result, consumed))
 }
 
 #[test]
 fn test_integer_decoder() {
     let mut buf: &[u8] = &[0x02];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(2));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((2, 1)));
     let mut buf: &[u8] = &[0x7f];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(127));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((127, 1)));
     let mut buf: &[u8] = &[0x80, 0x01];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(128));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((128, 2)));
     let mut buf: &[u8] = &[0x81, 0x01];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(129));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((129, 2)));
     let mut buf: &[u8] = &[0x82, 0x01];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(130));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((130, 2)));
     let mut buf: &[u8] = &[0xb9, 0x64];
-    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok(12857));
+    assert_eq!(decode_by_unsigned_leb128(buf.by_ref(), 32), Ok((12857, 2)));
 }
 
 pub fn parse(file_name: String) -> Result<Module, ParserErrorKind> {
