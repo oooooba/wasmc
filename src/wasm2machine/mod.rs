33889358dc96e160907e5bf45bdedb4adfb87d49
oooooba <fuumin.23@gmail.com>
2018-08-28 21:54:30 +0900

    feature: add feature to translate i32.irelop Wasm IR to machine IR

--- src/wasm2machine/mod.rs
+++ src/wasm2machine/mod.rs
@@ -486,6 +486,26 @@ impl WasmToMachine {
                 let new_block = Context::create_basic_block();
                 self.switch_current_basic_block_to(new_block);
             }
+            (&WasmInstr::Irelop(ref op), &WasmInstr::BrIf(index)) => {
+                let rhs = self.operand_stack.pop().unwrap();
+                let rhs_reg = rhs.get_as_register().unwrap();
+                let lhs = self.operand_stack.pop().unwrap();
+                let lhs_reg = lhs.get_as_register().unwrap();
+                let cond_kind = match op {
+                    &Irelop::Eq32 => JumpCondKind::Neq(lhs_reg, rhs_reg),
+                    &Irelop::Ne32 => JumpCondKind::Eq(lhs_reg, rhs_reg),
+                    &Irelop::LtS32 => JumpCondKind::GeS(lhs_reg, rhs_reg),
+                    &Irelop::LtU32 => JumpCondKind::GeU(lhs_reg, rhs_reg),
+                    &Irelop::GtU32 => JumpCondKind::LeU(lhs_reg, rhs_reg),
+                    &Irelop::LeS32 => JumpCondKind::GtS(lhs_reg, rhs_reg),
+                    &Irelop::GeU32 => JumpCondKind::LtU(lhs_reg, rhs_reg),
+                };
+                let entering_block = self.get_label_at(index.as_index());
+                self.emit_exiting_block(entering_block, cond_kind, false, true, false);
+
+                let new_block = Context::create_basic_block();
+                self.switch_current_basic_block_to(new_block);
+            }
             (
                 &WasmInstr::Irelop(ref op),
                 &WasmInstr::If(ref resulttype, ref then_instrs, ref else_instrs),
